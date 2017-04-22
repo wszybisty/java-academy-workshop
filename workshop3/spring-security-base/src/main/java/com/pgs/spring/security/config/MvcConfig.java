cdf1c358e7dc49ab98e3fb57862f5ddbc41ec991
wszybisty <wojciech.szybisty@gmail.com>
2017-04-22 13:02:39 +0200

    spring-security-base coded on workshop

--- workshop3/spring-security-base/src/main/java/com/pgs/spring/security/config/MvcConfig.java
+++ workshop3/spring-security-base/src/main/java/com/pgs/spring/security/config/MvcConfig.java
@@ -3,6 +3,7 @@ package com.pgs.spring.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.pgs.spring.security")
+@Import(SecurityConfig.class)
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Bean
