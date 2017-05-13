59cac62524128cd17bcc3803a6ebd7b993bce341
wszybisty <wojciech.szybisty@gmail.com>
2017-05-13 13:26:48 +0200

    user-registration coded on workshop

--- workshop4/user-registration/src/test/java/com/domain/spring/controller/UserControllerIT.java
+++ workshop4/user-registration/src/test/java/com/domain/spring/controller/UserControllerIT.java
@@ -1,15 +1,5 @@
 package com.domain.spring.controller;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.domain.spring.config.TestAppConfig;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
@@ -28,6 +18,15 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 /**
  *
  * @author wszybisty
@@ -35,8 +34,8 @@ import org.springframework.web.context.WebApplicationContext;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {TestAppConfig.class})
 @TestExecutionListeners({
-    DependencyInjectionTestExecutionListener.class,
-    DbUnitTestExecutionListener.class
+        DependencyInjectionTestExecutionListener.class,
+        DbUnitTestExecutionListener.class
 })
 @WebAppConfiguration
 public class UserControllerIT {
@@ -49,6 +48,67 @@ public class UserControllerIT {
     @Before
     public void setupTest() {
         mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    @DatabaseSetup("empty.ds.xml")
+    public void getUsers_emptyTable_shouldReturnNoRecords() throws Exception {
+        ResultActions perform = mvc.perform(get("/users"));
+
+        perform.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    @DatabaseSetup("UserControllerIT.ds.populated.xml")
+    public void getUsers_thereAreRecordsInUserTable_shouldReturnRecords() throws Exception {
+        ResultActions perform = mvc.perform(get("/users"));
+
+        perform.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].userId", equalTo(1)));
+    }
+
+    @Test
+    @DatabaseSetup("empty.ds.xml")
+    public void createUser_newUser_shouldInsertUser() throws Exception {
+        String userJson = "{\n"
+                + "	\"username\": \"new.user\",\n"
+                + "	\"firstName\": \"Jan\",\n"
+                + "	\"lastName\" : \"Kowalski\",\n"
+                + "	\"email\" : \"jan.kowalski@poczta.pl\",\n"
+                + "	\"phone\" : \"888999000\"\n"
+                + "}";
+
+        ResultActions perform = mvc.perform(post("/users").content(userJson).contentType(MediaType.APPLICATION_JSON));
+
+        perform.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.userId", notNullValue()));
+    }
+
+    @Test
+    @DatabaseSetup("UserControllerIT.ds.populated.xml")
+    public void createUser_existingUser_shouldReturBadRequest() throws Exception {
+        String userJson = "{\n"
+                + "	\"username\": \"user1\",\n"
+                + "	\"firstName\": \"Jan\",\n"
+                + "	\"lastName\" : \"Kowalski\",\n"
+                + "	\"email\" : \"jan.kowalski@poczta.pl\",\n"
+                + "	\"phone\" : \"888999000\"\n"
+                + "}";
+
+        ResultActions perform = mvc.perform(post("/users").content(userJson).contentType(MediaType.APPLICATION_JSON));
+
+        perform.andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.code", is("USER_ALREADY_EXISTS")));
     }
 
 }
--- workshop4/user-registration/src/test/java/com/domain/spring/service/UserRegistrationServiceTest.java
+++ workshop4/user-registration/src/test/java/com/domain/spring/service/UserRegistrationServiceTest.java
@@ -3,33 +3,89 @@ package com.domain.spring.service;
 import com.domain.spring.exception.UserRegistrationException;
 import com.domain.spring.model.User;
 import com.domain.spring.repository.UserRepository;
-import static org.assertj.core.api.Assertions.catchThrowable;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
 /**
  *
  * @author wszybisty
  */
+@RunWith(MockitoJUnitRunner.class)
 public class UserRegistrationServiceTest {
 
+    @Mock
     private NotificationService notificationService;
 
+    @Mock
     private UserRepository userRepository;
 
+    @InjectMocks
     private UserService userRegistrationService;
 
+    @Test
+    public void registerUser_userDoesNotExist_shouldSaveUserAndSendWelcomeMessage() throws UserRegistrationException {
+        User newUser = makeUser();
+        User savedUser = makeUser();
+        savedUser.setUserId(23L);
+        when(userRepository.save(newUser)).thenReturn(savedUser);
+
+        final User registeredUser = userRegistrationService.registerUser(newUser);
+
+        assertThat(registeredUser, is(notNullValue()));
+        assertThat(registeredUser.getUserId(), is(notNullValue()));
+
+        verify(notificationService).sendWelcomeMessage(any());
+        verifyNoMoreInteractions(notificationService);
+        //verifyZeroInteractions(notificationService);
+    }
+
+    @Test
+    public void registerUser_userAlreadyExists_shouldThrowException() {
+        User existingUser = makeUser();
+        when(userRepository.findByUsername(existingUser.getUsername()))
+                .thenReturn(makeUser());
+
+        final Throwable coughtException = catchThrowable(() -> userRegistrationService.registerUser(existingUser));
+
+        assertThat(coughtException, instanceOf(UserRegistrationException.class));
+        final UserRegistrationException userRegistrationException = (UserRegistrationException) coughtException;
+        assertThat(userRegistrationException.getFailReason(),
+                equalTo(UserRegistrationException.FailReason.USER_ALREADY_EXISTS));
+    }
+
+    @Captor
+    ArgumentCaptor<User> userCaptor;
+
+    @Test
+    public void registerUser_userDoesNotExist_shouldSaveUserAndSendWelcomeMessage2() throws UserRegistrationException {
+        User newUser = makeUser();
+        User savedUser = makeUser();
+        savedUser.setUserId(23L);
+        when(userRepository.save(newUser)).thenReturn(savedUser);
+
+        final User registeredUser = userRegistrationService.registerUser(newUser);
+
+        assertThat(registeredUser, is(notNullValue()));
+        assertThat(registeredUser.getUserId(), is(notNullValue()));
+
+        verify(notificationService).sendWelcomeMessage(userCaptor.capture());
+        assertThat(userCaptor.getValue().getUserId(), is(equalTo(23L)));
+        verifyNoMoreInteractions(notificationService);
+        //verifyZeroInteractions(notificationService);
+    }
 
     private User makeUser() {
         return new User(null, "janek.kowalski", "Jan", "Kowalski", "janek.kowalski@onet.pl", null);
